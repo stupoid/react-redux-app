7011b8b2e50a041b0905a95ef631c46628ba974b
stupoid <kelvin.kohkaiwen@g.hde.co.jp>
2017-06-02 18:46:25 +0900

    login form partially works=

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
+import Authenticate from '../authenticate'
 
 const App = () => (
   <div>
     <header>
       <Link to="/">Home</Link>
       <Link to="/about-us">About</Link>
+      <Link to="/login">Login</Link>
     </header>
 
     <main>
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
+      <Route exact path="/login" component={Authenticate} />
     </main>
   </div>
 )
